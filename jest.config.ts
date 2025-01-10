c2513b534e8027a526e362bc9d97a793b5a7589f
Tal Jacob <taljacob2@gmail.com>
2025-01-10 13:50:40 +0200

    Add Extra Missing Parameters For Jest Config

--- jest.config.ts
+++ jest.config.ts
@@ -5,8 +5,13 @@
 
 import type {Config} from 'jest';
 
+
 const config: Config = {
   testTimeout: 10 * 1000,
+  passWithNoTests: true,
+  watchAll: false,
+  detectOpenHandles: true,
+  forceExit: true,
 
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -140,7 +145,7 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ['./src/tests/setup-tests.ts'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
